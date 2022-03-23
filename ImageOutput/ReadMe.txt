6ffe24003412dec7218bea2bfdbab083d0b4ca58
Tucker Higgins <thiggin6@lion.lmu.edu>
2022-03-23 16:59:45 -0700

    .

---
+++ ImageOutput/ReadMe.txt
@@ -0,0 +1,1 @@
+﻿This is the output folder of finished atlas'
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,9 +26,16 @@ namespace AtlasPadder
         public MainWindow()
         {
             InitializeComponent();
+
         }
 
-        public BitmapImage BitMapImage { get; set; }
+        private BitmapImage _bitMapImage;
+        private string _fileNameSafe;
+
+        private int _imageWidth;
+        private int _imageHeight;
+
+        private int _tileSize = 16;
 
         private void BrowseButton_Click(object sender, RoutedEventArgs e)
         {
@@ -38,28 +47,51 @@ namespace AtlasPadder
             if (dlg.ShowDialog() == true)
             {
                 string selectedFileName = dlg.FileName;
+                _fileNameSafe = dlg.SafeFileName;
                 FileNameLabel.Content = selectedFileName;
-                BitMapImage = new BitmapImage();
-                BitMapImage.BeginInit();
-                BitMapImage.UriSource = new Uri(selectedFileName);
-                BitMapImage.EndInit();
-                ImageViewer1.Source = BitMapImage;
+                _bitMapImage = new BitmapImage();
+                _bitMapImage.BeginInit();
+                _bitMapImage.UriSource = new Uri(selectedFileName);
+                _bitMapImage.EndInit();
+                ImageViewer1.Source = _bitMapImage;
 
-              
+
                 Process.IsEnabled = true;
             }
         }
 
+        private void ProcessButton_Click(object sender, RoutedEventArgs e)
+        {
+            Bitmap bitMap;
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(_bitMapImage));
+                enc.Save(outStream);
+                System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(outStream);
+
+                bitMap = new Bitmap(bitmap);
+            }
+
+            _imageWidth = bitMap.Width;
+            _imageHeight = bitMap.Height;
+
+
+
+
+            SaveBitMapImage();
+        }
         private void SaveBitMapImage()
         {
-            BitmapEncoder encoder = new PngBitmapEncoder();
-            encoder.Frames.Add(BitmapFrame.Create(BitMapImage));
+            string path = $"{System.AppDomain.CurrentDomain.BaseDirectory}ImageOutput/{_fileNameSafe}";
 
-            using (var fileStream = new System.IO.FileStream(filePath, System.IO.FileMode.Create))
+            BitmapEncoder encoder = new PngBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(_bitMapImage));
+            using (var fileStream = new System.IO.FileStream(path, System.IO.FileMode.Create))
             {
                 encoder.Save(fileStream);
             }
         }
-    
+
     }
 }
